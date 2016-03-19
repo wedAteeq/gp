585f0c6fa291fcb051f55449ab314e140616d030
AAreej <areej.khaled.j@gmail.com>
2016-03-19 15:43:16 +0300

    I change names of objects to prevent Confused and I add add and delete functhions Areej

--- 3DexCity/Assets/Scripts/Room.cs
+++ 3DexCity/Assets/Scripts/Room.cs
@@ -1,15 +1,56 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEditor;
+using System.Text;
 
-public class Room  {
+public class Room : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+   
+    public Collider FramePic; //referrnse to frame of picture 
+    public string filePath = ""; //the path of the picture 
+    public Texture2D texture; //the picture itself
+
+    //Manage Room (Add/Delete/Like/Arrang contents) 
+
+
+    void OnTriggerEnter(Collider other) // to know which fram will be affect 
+
+    {
+        if (other.gameObject.CompareTag("Frame"))  // we will know from tag 
+
+        {
+            other.gameObject.SetActive(false);
+            FramePic = other;
+        }
+    }
+
+    public void AddContents()
+
+    {
+#if UNITY_EDITOR
+        filePath = EditorUtility.OpenFilePanel("Overwrite with png"
+                                            , Application.streamingAssetsPath
+                                            , "png");
+#endif
+        if (filePath.Length != 0)
+        {
+            WWW www = new WWW("file://" + filePath);
+            texture = new Texture2D(64, 64);
+            www.LoadImageIntoTexture(texture);
+            FramePic.GetComponent<Renderer>().material.mainTexture = texture;
+
+        }
+
+    }
+
+
+    public void DeleteContents()
+
+    {
+        FramePic.GetComponent<Renderer>().material.mainTexture = null;
+    }
+
+
+
 }
